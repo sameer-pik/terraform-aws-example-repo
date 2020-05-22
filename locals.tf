3a3dfd06caeba40922503bfac0f15f96edd25fea
Sameer Sathar <51897064+sameersathar@users.noreply.github.com>
2020-05-22 10:47:17 +0800

    local values for this stack

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+  vpc_id               = "${var.vpc_id}"
+  environment          = "${var.environment}"
+  product_domain       = "${var.product_domain}"
+  service_name         = "${var.service_name}"
+  team                 = "${var.team}"
+  key_name             = "${var.ec2_key_name}"
+  role                 = "${var.role}"
+  }
